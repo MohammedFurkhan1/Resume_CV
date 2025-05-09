3dc7081716de0a0345585ccf8be05385b5b56225
MohammedFurkhan1 <86548017+MohammedFurkhan1@users.noreply.github.com>
2025-05-10 00:49:36 +0530

    Update resume_cv.py

--- resume_cv.py
+++ resume_cv.py
@@ -131,6 +131,5 @@ st.markdown("""
 #contact
 st.markdown("""
 <div class='section-title'>CONTACT</div>
-st.markdown("""
-<div class='section-title'>Mail :</div>""", unsafe_allow_html=True) mohammedfurkhan073@gmail.com | Mobile : +91 8217405450 | Location : Hyderabad ,India
+<strong> Mail :  </strong> mohammedfurkhan073@gmail.com | Mobile : +91 8217405450 | Location : Hyderabad ,India
 """, unsafe_allow_html=True)
